af4cc0e041b48f3cbf4703aa9bc19d11b7b6f8f6
Milad Rafigh <milad.gold2021@gmail.com>
2026-07-25 19:08:46 +0330

    feat: implement user registration api

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,28 +1,58 @@
 import type { Request, Response } from "express";
+import { registerUser } from "../services/auth.service.js";
+
 
 export const register = async (
   req: Request,
   res: Response
 ) => {
-  res.json({
-    message: "Register endpoint"
-  });
+
+  try {
+
+    const user = await registerUser(req.body);
+
+
+    return res.status(201).json({
+      message: "User created successfully",
+      user,
+    });
+
+
+  } catch (error) {
+
+    return res.status(400).json({
+      message:
+        error instanceof Error
+          ? error.message
+          : "Something went wrong",
+    });
+
+  }
+
 };
+
+
 
 export const login = async (
   req: Request,
   res: Response
 ) => {
+
   res.json({
     message: "Login endpoint"
   });
+
 };
+
+
 
 export const me = async (
   req: Request,
   res: Response
 ) => {
+
   res.json({
     message: "Current user"
   });
+
 };
